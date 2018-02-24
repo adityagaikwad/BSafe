cb3863db1a22fee3b29364b62a4ecb2b429abb4d
Meet <meetgopani1996@gmail.com>
2018-02-24 21:32:17 +0530

    api done

--- borderify.js
+++ borderify.js
@@ -1,3 +1,21 @@
 document.body.style.border = "5px solid red";
 
 console.log(document.location.href);
+
+var url = document.location.href;
+$(function () {
+console.log("Hello World");
+	
+	$.ajax({
+		url: "main.php",
+		dataType: 'json',
+		type: "POST",
+		data : {url : url},
+		success :function (result) {
+			console.log(result);
+		},
+		error : function(){
+			console.log("error");
+		},
+	});
+});
---
+++ main.php
@@ -0,0 +1,61 @@
+
+<?php
+ if (isset($_POST["url"]) && !empty($_POST["url"]))
+  { //Checks if action value exists
+    $url = $_POST['url'];
+}
+else{
+     $url = " http://www.ixxi.com.mx/pp/log-in/customer_center/customer-IDPP00C877/myaccount/signin/ ";
+}   
+    $url = trim($url);
+    $debug = false;
+    $url = strtolower($url);
+// $url = urlencode($url);
+
+    $url_parsed = parse_url($url);
+// print_r($url_parsed);
+    $url = $url_parsed['host']; 
+    if (isset($url_parsed['path'])) $url .= $url_parsed['path'];
+    if (isset($url_parsed['query'])) $url .= $url_parsed['query'];
+$url = urldecode($url); //remove hex encodings
+$url = preg_replace('/^\.*/','', $url); //remove leading dots
+$url = preg_replace('/\.*$/','', $url); //remove trailing dots
+$url = preg_replace('/\.+/','.', $url); //replace consecutive dots
+$url = preg_replace('/\/+/','/', $url); //replace consecutive slashes
+// echo $url;
+$url = iconv(mb_detect_encoding($url, mb_detect_order(), true), "UTF-8", $url);
+$url = urlencode($url);
+// print_r($url);
+$has_resource = false;
+if (isset($url_parsed['path'])) {
+    $path = $url_parsed['path'];
+    $path_array = split('/',$path);
+    $target = $path_array[count($path_array)-1];
+    if (! strpos($target,'.') && substr($target,-1)!='/') $url .= '/';
+    else $has_resource = true;
+}
+
+
+if ($debug){ echo "<p>URL: $url</p>";}
+
+    if ($debug){ echo "<p>Path: ".$url_parsed['path']."</p>";}
+
+
+        $API_KEY = "YOUR_API_KEY";
+        $url_final = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=helloworld&key=" . $API_KEY . "&appver=1.5.2&pver=3.1&url=" . $url;
+        $ch = curl_init();
+        // echo $url_final;
+        $timeout = 5;
+        curl_setopt($ch,CURLOPT_URL,$url_final);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        // echo 'HTTP code: ' . $httpcode;
+        if ($httpcode != 200){
+            $data = "ok";
+        }
+        echo json_encode($data);
+        ?>
