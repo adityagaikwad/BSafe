9a396bc8dc90e41f52fa4f9f4aca25708b53dbb6
aditya <adityagaikwad009@gmail.com>
2018-02-25 13:32:17 +0530

    testing

--- borderify.js
+++ borderify.js
@@ -1,21 +1,18 @@
 document.body.style.border = "5px solid red";
 
 console.log(document.location.href);
-
 var url = document.location.href;
-$(function () {
-console.log("Hello World");
-	
-	$.ajax({
-		url: "main.php",
-		dataType: 'json',
-		type: "POST",
-		data : {url : url},
-		success :function (result) {
-			console.log(result);
-		},
-		error : function(){
-			console.log("error");
-		},
+
+jQuery.ajax({
+	type: "POST",
+	url: 'main.php',
+	dataType: 'json',
+	data: {"url": url},
+
+	success :function (result) {
+		console.log(result);
+	}, 
+	error : function(){
+		console.log("error");
+
 	});
-});
