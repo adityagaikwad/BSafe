a5886c544553f5fbd19c95a47b74556d2d7a77f1
name <adityagaikwad009@gmail.com>
2018-03-19 21:36:49 +0530

    Working

--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -4,46 +4,45 @@
    * If this content script is injected into the same page again,
    * it will do nothing next time.
    */
-  if (window.hasRun) {
+   if (window.hasRun) {
     return;
   }
+
+  // load jquery library
   window.hasRun = true;
-
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
-  function insertBeast(beastURL) {
-    removeExistingBeasts();
-    let beastImage = document.createElement("img");
-    beastImage.setAttribute("src", beastURL);
-    beastImage.style.height = "100vh";
-    beastImage.className = "beastify-image";
-    document.body.appendChild(beastImage);
-  }
-
-  /**
-   * Remove every beast from the page.
-   */
-  function removeExistingBeasts() {
-    let existingBeasts = document.querySelectorAll(".beastify-image");
-    for (let beast of existingBeasts) {
-      beast.remove();
-    }
-  }
+  (function(){
+    var newscript = document.createElement('script');
+    newscript.type = 'text/javascript';
+    newscript.async = true;
+    newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+    (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+  })();
 
   /**
    * Listen for messages from the background script.
    * Call "beastify()" or "reset()".
-  */
-  browser.runtime.onMessage.addListener((message) => {
+   */
+   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "beastify") {
-      insertBeast(message.beastURL);
-    } else if (message.command === "reset") {
-      removeExistingBeasts();
+      // insertBeast(message.beastURL);
+      var xhr = new XMLHttpRequest();
+      // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.open("POST", "http://127.0.0.1/Plugin/main.php", true);
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.responseText);
+          // alert("lol");
+      }
+      // else {
+      //   console.log("wrong");
+      // }
     }
-  });
+    var url2 = document.location.href;
+    console.log(url2);
+    xhr.send("url=url2");
+  } else if (message.command === "reset") {
+    // removeExistingBeasts();
+  }
+});
 
-
-})();
+ })();
